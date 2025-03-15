2eac322883c7dc09586516e46041ce61b1353e1f
MarcosAltec <marcospassos2211@gmail.com>
2025-03-14 21:22:11 -0300

    subindo pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,20 @@
+import { View } from "react-native";
+import ListScreen from "./screens/ListScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import { HomeScreen } from "./screens/HomeScreen";
+import { ProfileScreen } from "./screens/ProfileScreen";
+
+const App = () => {
+  return(
+    <>
+      {/*  */}
+      {/*  */}
+      <ListScreen />
+      <GalleryScreen />
+      <HomeScreen />
+      <ProfileScreen />
+    </>
+  )
+}
+
+export default App;
