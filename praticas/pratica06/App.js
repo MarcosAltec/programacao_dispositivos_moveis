19d19daab265c1e17c1cff958090220f92961a5d
MarcosAltec <marcospassos2211@gmail.com>
2025-04-07 20:59:32 -0300

    subindo pratica 06

---
+++ praticas/pratica06/App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TaskProvider } from "./contexts/TaskContext";
+import TaskScreen from "./screens/TaskScreen";
+
+function App() {
+  return(
+    <SafeAreaProvider>
+        <TaskProvider>
+          <TaskScreen />
+        </TaskProvider>
+    </SafeAreaProvider>
+  )
+}
+
+export default App;
