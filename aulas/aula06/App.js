161eee7b2c9f734b0d5547d087d7da79340c4752
MarcosAltec <marcospassos2211@gmail.com>
2025-04-25 21:50:36 -0300

    subindo aula06

---
+++ aulas/aula06/App.js
@@ -0,0 +1,17 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import NavegacaoPilha from './routes/NavegacaoPilha';
+import NavegacaoAbaInferior from './routes/NavegacaoAbaInferior';
+import NavegacaoAbaSuperior from './routes/NavegacaoAbaSuperior';
+import NavegacaoGaveta from './routes/NavegacaoGaveta';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <NavegacaoGaveta />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
