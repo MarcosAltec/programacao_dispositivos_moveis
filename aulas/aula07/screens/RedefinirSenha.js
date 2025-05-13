c33fb3cebe7047a4bc1fe42e6e0a25a3b632c59e
MarcosAltec <marcospassos2211@gmail.com>
2025-05-12 21:36:29 -0300

    subindo conclusão da aula07

---
+++ aulas/aula07/screens/RedefinirSenha.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { TextInput, Button, HelperText } from "react-native-paper";
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+
+function RedefinirSenha() {
+  const schema = Yup.object().shape({
+    // nome: Yup.string().required("Nome é obrigatório"),
+    email: Yup.string().required("E-mail é obrigatório").email("E-mail é inválido"),
+    // senha: Yup.string().required("Senha é obrigatório").min(8, "Deve ter pelo menos 8 caracteres"),
+    // confirmaSenha: Yup.string().oneOf([Yup.ref("senha"), null], "As senhas não coincidem")
+});
+  const [email, setEmail] = useState("");
+  const [erro, setErro] = useState("");
+
+  // const validaEmail = () => {
+  //   if (!email) {
+  //     setErro("E-mail é obrigatorio");
+  //     return false;
+  //   }
+
+  //   if (!email.includes("@")) {
+  //     setErro("E-mail inválido");
+  //     return false;
+  //   }
+
+  //   return true;
+  // };
+
+  const trataTexto = async (texto) => {
+    try {
+      setEmail(texto);
+      await schema.validate({ email: texto });
+      setErro("")
+    } catch(err) {
+      setErro(err.errors);
+    }
+  }
+
+  const trataEnviar = async () => {
+    try{
+      await schema.validate({ email });
+      setErro("");
+      setEmail("");
+      alert("E-mail enviado!");
+    } catch(err) {
+      setErro(err.errors);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <TextInput
+        autoCapitalize="none"
+        keyboardType="email-address"
+        label="Email"
+        value={email}
+        onChangeText={trataTexto}
+        mode="outlined"
+        error={erro != ""}
+      />
+      <HelperText type="error" visible={erro != ""}>
+        {erro}
+      </HelperText>
+      <Button mode="contained" onPress={trataEnviar}>
+        Enviar
+      </Button>
+    </View>
+  );
+}
+
+export default RedefinirSenha;
