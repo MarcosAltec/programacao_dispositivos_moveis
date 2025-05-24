1d607f2b674cb5d4de2bc13df8f2a9e890f34f11
MarcosAltec <marcospassos2211@gmail.com>
2025-05-23 21:47:03 -0300

    subindo aula 08

---
+++ aulas/aula08/components/Grid.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { View, FlatList } from "react-native";
+import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { LojaContext } from "../contexts/LojaContext";
+
+function Grid() {
+  const navigation = useNavigation();
+  const {categorias} = useContext(LojaContext);
+
+  return (
+    <View>
+      <FlatList
+        data={categorias}
+        numColumns={2}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          <View style={{ width: "48%", padding: 8 }}>
+            <Button
+              mode="contained"
+              icon={item.icone}
+              onPress={() => navigation.navigate("Lojas", { categoria: item.nome })}
+            >
+              {item.nome}
+            </Button>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+export default Grid;
---
+++ aulas/aula08/components/ListaHorizontal.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { View, FlatList } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { LojaContext } from "../contexts/LojaContext";
+
+function ListaHorizontal() {
+  const {categorias} = useContext(LojaContext);
+
+  return (
+    <View>
+      <Text variant="titleMedium">Categorias</Text>
+      <FlatList
+        data={categorias}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtactor={(item, index) => index}
+        renderItem={({ item }) => (
+            <View style={{flex: 1, marginRight: 8}}>
+            <Button mode="outlined" icon={item.icone}>
+                {item.nome}
+            </Button>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+export default ListaHorizontal;
