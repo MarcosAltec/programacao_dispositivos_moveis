b867e7b702236ec1ae8dc9738728ce2fc1a0fc11
MarcosAltec <marcospassos2211@gmail.com>
2025-06-13 20:25:45 -0300

    subindo aula11

--- aulas/aula11/screens/Home.js
+++ aulas/aula11/screens/Home.js
@@ -1,15 +1,18 @@
-import { useContext } from 'react';
-import { View } from 'react-native'
-import { AuthContext } from '../contexts/AuthContext';
-import { Button } from 'react-native-paper';
+import { useContext } from "react";
+import { View } from "react-native";
+import { Text, Button } from "react-native-paper";
+import { AuthContext } from "../contexts/AuthContext";
 
 function Home() {
-    const {usuario, logout} = useContext(AuthContext);
-
-    return <View style={{flex: 1, padding: 16, justifyContent: "space-around"}}>
-        <Text variant='titleLarge'>Olá, {usuario.login}</Text>
-        <Button mode='contained' onPress={() => logout()}>Sair</Button>
+  const { usuario, logout } = useContext(AuthContext);
+  return (
+    <View style={{ flex: 1, padding: 16, justifyContent: "space-around" }}>
+      <Text variant="titleLarge">Olá, {usuario.login}</Text>
+      <Button mode="contained" onPress={() => logout()}>
+        Sair
+      </Button>
     </View>
-};
+  );
+}
 
 export default Home;
